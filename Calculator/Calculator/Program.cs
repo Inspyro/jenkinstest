6b4859bdb0cf671784aae0dc4c277e75897bb8cd
Patrick Schwarz <patrick.schwarz@rubicon.eu>
2020-08-03 10:39:17 +0200

    Add simple donet core project for testing purposes

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Calculator.exe");
+            Console.ReadKey();
+        }
+    }
+}
